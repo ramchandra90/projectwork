ac5d7f01e4b0a8746fb36fefb4113bd4a4f6d038
ramachandra <ramchandra081@gmail.com>
2025-06-03 18:57:31 +1200

    functions

--- EKS-install/main.tf
+++ EKS-install/main.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "demo-terraform-eks-state-s3-bucket"
     key            = "terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-eks-state-locks"
+  
     encrypt        = true
     use_lockfile   = true
   }
