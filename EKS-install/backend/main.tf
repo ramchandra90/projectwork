ecb052c7b7c8b022a99e7cba643728ea29a5c8ad
ramachandra <ramchandra081@gmail.com>
2025-06-03 18:48:00 +1200

    functions

--- EKS-install/backend/main.tf
+++ EKS-install/backend/main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
   versioning_configuration {
     status = "Enabled"
   }
-}.addOverride("use_lockfile", true)
+}
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
